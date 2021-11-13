b3812363f2469767a9b2a0c99a8ae30e092ebdd5
Michael Lock <thelockymichael@gmail.com>
2021-11-13 11:24:34 +0200

    changed favicon.ico

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import "./App.css";
 
-function App() {
+const App: React.FC<{}> = () => {
+  // TODO
+  // Router
+  // Switch
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Hello world !!!</p>
-      </header>
+    <div>
+      <p>Hello world !!!</p>
     </div>
   );
-}
+};
 
 export default App;
