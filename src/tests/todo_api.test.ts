effb24792f2847562b76d41a8919222a08d462bc
Michael Lock <thelockymichael@gmail.com>
2021-11-27 13:36:39 +0200

    todo tests refactoring

--- src/tests/todo_api.test.ts
+++ src/tests/todo_api.test.ts
@@ -8,64 +8,62 @@ const api = supertest(app);
 
 beforeEach(async () => {
   await Todo.deleteMany({});
-
-  const todoObjects = helper.initialTodos.map((todo) => Todo.build(todo));
-  const promiseArray = todoObjects.map((todo) => todo.save());
-  await Promise.all(promiseArray);
-
-  // let todoObject = Todo.build(helper.initialTodos[0]);
-  // await todoObject.save();
-
-  // todoObject = Todo.build(helper.initialTodos[1]);
-  // await todoObject.save();
-
-  // todoObject = Todo.build(helper.initialTodos[2]);
-  // await todoObject.save();
+  await Todo.insertMany(helper.initialTodos);
 });
 
-test("all todos are returned", async () => {
-  const response = await api.get("/api/todo");
+describe("when there is initially some todos saved", () => {
+  test("all todos are returned", async () => {
+    const response = await api.get("/api/todo");
 
-  expect(response.body).toHaveLength(helper.initialTodos.length);
+    expect(response.body).toHaveLength(helper.initialTodos.length);
+  });
+
+  test("a specific todo is within the returned todos", async () => {
+    const response = await api.get("/api/todo");
+
+    const contents = response.body.map((r: ITodo) => r.title);
+    expect(contents).toContain("Do the dishes");
+  });
 });
 
-test("a specific todo is within the returned todos", async () => {
-  const response = await api.get("/api/todo");
+// TODO
+// viewing a specific note'
+// succeeds with a valid id
+// fails with statuscode 404 if note does not exist
+// fails with statuscode 400 id is invalid
 
-  const contents = response.body.map((r: ITodo) => r.title);
-  expect(contents).toContain("Do the dishes");
-});
+describe("addition of a new note", () => {
+  test("succeeds with valid data", async () => {
+    const newTodo = {
+      title: "async/await simplifies making async calls",
+      description: "This is true",
+    };
 
-test("a valid todo can be added", async () => {
-  const newTodo = {
-    title: "async/await simplifies making async calls",
-    description: "This is true",
-  };
+    await api
+      .post("/api/todo/")
+      .send(newTodo)
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
 
-  await api
-    .post("/api/todo/")
-    .send(newTodo)
-    .expect(200)
-    .expect("Content-Type", /application\/json/);
+    const response = await api.get("/api/todo");
 
-  const response = await api.get("/api/todo");
+    const titles = response.body.map((r) => r.title);
 
-  const titles = response.body.map((r) => r.title);
+    expect(response.body).toHaveLength(helper.initialTodos.length + 1);
+    expect(titles).toContain("async/await simplifies making async calls");
+  });
 
-  expect(response.body).toHaveLength(helper.initialTodos.length + 1);
-  expect(titles).toContain("async/await simplifies making async calls");
-});
+  test("fails with status code 400 if todo data invaild", async () => {
+    const newTodo = {
+      description: "This should fail.",
+    };
 
-test("todo without title is not added", async () => {
-  const newTodo = {
-    description: "This should fail.",
-  };
+    await api.post("/api/todo").send(newTodo).expect(400);
 
-  await api.post("/api/todo").send(newTodo).expect(400);
+    const response = await api.get("/api/todo");
 
-  const response = await api.get("/api/todo");
-
-  expect(response.body).toHaveLength(helper.initialTodos.length);
+    expect(response.body).toHaveLength(helper.initialTodos.length);
+  });
 });
 
 afterAll(() => {
